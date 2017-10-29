a2261fed1aaeb5179e0b5d5eb268d4108ddea8d5
onebadger <yakov.barsukov@gmail.com>
2017-10-29 15:23:34 +0300

    files update

--- js/app.js
+++ js/app.js
@@ -1,10 +1,28 @@
 // show and fix hiden top on scroll down more then Xpx
 
 $(window).bind('scroll', function () {
-  if ($(window).scrollTop() > 400) {
-    $('#fixed-top ').css('display', 'flex');
+  if ($(window).scrollTop() > 700) {
+    $('#fixed-top').css('display', 'block');
   } else {
-    $('#fixed-top ').css('display', 'none');
+    $('#fixed-top').css('display', 'none');
   }
 });
+
+
+// slow scrollTop function
+
+$(document).ready(function(){  
+  $("#fixed-top-img a").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $("#header").offset().top
+    }, 800);
+  });
+});
+
+// responsive img gallery
+
+// check html loaded. load X-pictures img gallery due to css condition set
+
+
 
